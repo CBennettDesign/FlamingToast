c6318ad685b728135391a27485e357afde668067
Alex Scicluna <alex.scicluna@yahoo.com.au>
2017-11-08 09:05:50 +1100

    v1.91 Added BaseCanisterSnapper, forgot to do that in the last push.

---
+++ Flaming Toast/UnityProject/FinalProject/Assets/Scripts/Alex/BaseCanisterSnapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCanisterSnapper : MonoBehaviour {
+
+
+    public FluxType[] compatibleTypes;
+
+    public virtual bool IsCompatibleCanister(Canister a_canister )
+    { 
+        foreach(FluxType t in compatibleTypes)
+        {
+            if (a_canister.Type == t)
+                return true;
+        }
+
+        return false;
+    }
+
+    public virtual bool giveCanister(GameObject a_canister)
+    {
+        return false;
+    }
+}
